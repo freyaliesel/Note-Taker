20c791a713db9d16b4f4f52e2bf8882483e37906
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-04-11 15:44:34 -0500

    fixed typo

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { v4: uuidv4 } = require("uuid");
 const { fstat } = require("fs");
 
 const app = express();
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
